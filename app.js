12413b39ea175f397d96bc0e66f6468eda67d962
oreanroy <oreanroy@gmail.com>
2018-12-29 19:56:11 +0530

    cors handler added

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-requested-With, Content_Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
 });
 
 //routes which should handle requests
