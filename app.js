20895a4abee7f3b9b60ee0938c9e5de9295f7010
oreanroy <oreanroy@gmail.com>
2018-12-29 16:48:48 +0530

    initial commit added get post push delte request handler

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+});
+
+//routes which should handle requests
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+
+
+module.exports = app;
