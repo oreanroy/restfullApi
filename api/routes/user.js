6cb4f827b6c97aa2f86bb52aeebc6a286d0f61b4
oreanroy <oreanroy@gmail.com>
2019-01-01 23:23:00 +0530

    email validation before creating mongo object added

--- api/routes/user.js
+++ api/routes/user.js
@@ -47,4 +47,20 @@ router.post('/signup', (req, res, next) => {
 });
 
 
+router.delete('/:userId', (req, res, next) => {
+    User.remove({_id: req.params.userId})
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: "user deleted"
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+})
+
 module.exports = router;
