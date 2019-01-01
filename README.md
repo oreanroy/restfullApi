7af5f26f965fbd87d1d504a2def51936d6162125
oreanroy <oreanroy@gmail.com>
2019-01-01 22:52:16 +0530

    user signup work, dublicate user adding not allowed

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # restfullApi
-A node and express based restful API.
+A node and express based restful API, with monogo atlas db.
 <br>
+<h3>Dependencies</h3>
+<p><strong>morgan</strong> to log request.</p>
+<br/>
+<p><strong>mongoose</strong> easy connection and read write operation on mongo</p>
+<br>
+<p><strong>multer</strong> to upload files</p>
+<br>
+<p><strong>node brcypt</strong> to hash and salt pasword </p> 
 <h4> End Points</h4>
 <p>Post request to add a product with the api defined body structure</p>
 <a href="http://localhost:3000/products/">http://localhost:3000/products/</a>
--- api/routes/user.js
+++ api/routes/user.js
@@ -6,32 +6,44 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            });
-        }else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash 
-            });
-            user.save()
-                .then(result => {
-                    console.log(result)
-                    res.status(201).json({
-                        message: 'User created'
-                    });
+    User.find({email: req.body.email})
+        .exec()
+        .then( user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Email with same name exist"
                 })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
+            }else{
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        console.log("this is the error")
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash 
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result)
+                                res.status(201).json({
+                                    message: 'User created'
+                                });
+                            })
+                            .catch(err => {
+                                console.log("an error occured here!! haha")
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                            });
+                        });
+                    } 
                 });
-            });
-        } 
-    });
+            }
+        })
 });
 
 
