06da7bdb8cefabca8405c96dcd51474c04c8bdd5
oreanroy <oreanroy@gmail.com>
2019-01-02 14:40:28 +0530

    webtoken creater added

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # restfullApi
-A node and express based restful API, with monogo atlas db.
+A node and express based restful API, with monogo atlas for data storage.
 <br>
 <h3>Dependencies</h3>
 <p><strong>morgan</strong> to log request.</p>
-<br/>
 <p><strong>mongoose</strong> easy connection and read write operation on mongo</p>
-<br>
 <p><strong>multer</strong> to upload files</p>
-<br>
-<p><strong>node brcypt</strong> to hash and salt pasword </p> 
-<h4> End Points</h4>
-<p>Post request to add a product with the api defined body structure</p>
+<p><strong>node brcypt</strong> to hash and salt password </p> 
+<h3> End Points</h3>
+<h4>POST</h4>
+<p>To add a product with the api defined body structure</p>
 <a href="http://localhost:3000/products/">http://localhost:3000/products/</a>
 <br>
+<p>To create an order in the defined body structure</p>
+<a href="http://localhost:3000/orders">http://localhost:3000/orders</a>
+
 <p>Get request to get all the avialabe products</p>
 <a href="http://localhost:3000/products/">http://localhost:3000/products/</a>
 <br>
+
 
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -46,6 +47,49 @@ router.post('/signup', (req, res, next) => {
         })
 });
 
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email})
+        .exec()
+        .then(user => {
+            if(user.lenght < 1) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err){
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign(
+                    {
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, 
+                    process.env.JWT_KEY, 
+                    {
+                        expiresIn: "1h"
+                    }
+                    );
+                    return res.status(200).json({
+                        message: "Auth successfull",
+                        token: token
+                    });
+                }
+                res.status(401).json({
+                    message: "Auth failed"
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
 
 router.delete('/:userId', (req, res, next) => {
     User.remove({_id: req.params.userId})
